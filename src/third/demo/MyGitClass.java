6c3d1ba66a70761ecde9f6c6ad2b097f2fbd1a5f
naseer <masoodi07@gmail.com>
2023-08-25 05:05:24 -0700

    description

---
+++ src/third/demo/MyGitClass.java
@@ -0,0 +1,12 @@
+package third.demo;
+
+public class MyGitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("what is this");
+		
+
+	}
+
+}
